13d03ebc5f3dee667611a7945f8c0582e7d6749e
Adriana Cavalcanti <drii.cavalcanti@gmail.com>
2019-02-03 20:02:48 -0200

    preparando div para o grafo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,44 @@
-function write_text() {
-  document.getElementById(22).innerHTML = document.getElementById(7).value;
-};
+$( document ).ready(function() {
+   var cy = cytoscape({
+
+  container: $('#div_graph'), // container to render in
+
+  elements: [ // list of graph elements to start with
+    { // node a
+      data: { id: 'a' }
+    },
+    { // node b
+      data: { id: 'b' }
+    },
+    { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    }
+  ],
+
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+
+  layout: {
+    name: 'grid',
+    rows: 1
+  }
+
+});
+});
