81f31a027aae55200323afe9bcc99ed4cf86be18
Adriana Cavalcanti <drii.cavalcanti@gmail.com>
2019-02-03 19:19:18 -0200

    Teste gerando um graph

--- scripts.js
+++ scripts.js
@@ -1,3 +1,50 @@
 function write_text() {
   alert('Hello World');
 };
+
+
+var cy = cytoscape({
+
+  container: document.getElementById('div_grah'), // container to render in
+
+  elements: [ // list of graph elements to start with
+    { // node a
+      data: { id: 'a' }
+    },
+    { // node b
+      data: { id: 'b' }
+    },
+    { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    }
+  ],
+
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+
+  layout: {
+    name: 'grid',
+    rows: 1
+
+
+  }
+
+});
+
